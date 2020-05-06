adff4e8702d7ebbf7ecac3e6f586a3a7317423b2
Jon Jagger <jon@jaggersoft.com>
2020-05-06 11:45:17 +0100

    In Tennis-Java put options on separate lines in cyber-dojo.sh

--- start-points/Tennis/Java/cyber-dojo.sh
+++ start-points/Tennis/Java/cyber-dojo.sh
@@ -5,12 +5,19 @@ cd ${CYBER_DOJO_SANDBOX}
 
 CLASSES=.:`ls /junit/*.jar | tr '\n' ':'`
 
-if javac --enable-preview --release 14 -Xlint:preview -Xlint:unchecked -Xlint:deprecation -cp $CLASSES *.java; then
-  java --enable-preview -jar /junit/junit-platform-console-standalone-1.6.2.jar \
-      --disable-banner \
-      --disable-ansi-colors \
-      --details=tree \
-      --details-theme=ascii \
-      --class-path . \
-      --scan-class-path
+if javac --enable-preview \
+         --release 14 \
+         -Xlint:preview \
+         -Xlint:unchecked \
+         -Xlint:deprecation \
+         -cp $CLASSES *.java
+then
+  java --enable-preview \
+       -jar /junit/junit-platform-console-standalone-1.6.2.jar \
+       --disable-banner \
+       --disable-ansi-colors \
+       --details=tree \
+       --details-theme=ascii \
+       --class-path . \
+       --scan-class-path
 fi
