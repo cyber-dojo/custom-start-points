460c258a4fc14782d42a9fa6286678aa4637a3d6
Jon Jagger <jon@jaggersoft.com>
2020-06-05 07:59:11 +0100

    Split off helm-upgrade into its own script

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -1,17 +1,5 @@
 #!/bin/bash -Eeu
 
-# Normally I export the cyberdojo env-vars using the command
-# $ docker run --rm cyberdojo/versioner:latest
-# This won't work on the.circleci deployment step since it is
-# run inside the cyberdojo/gcloud-kubectl-helm image which does
-# not have docker. So doing it directly from versioner's git repo
-export $(curl https://raw.githubusercontent.com/cyber-dojo/versioner/master/app/.env)
-
-readonly NAMESPACE="${1}" # beta | prod
-readonly IMAGE="${CYBER_DOJO_CUSTOM_START_POINTS_IMAGE}"
-readonly PORT="${CYBER_DOJO_CUSTOM_START_POINTS_PORT}"
-readonly TAG="${CIRCLE_SHA1:0:7}"
-
 # misc env-vars are in ci context
 
 echo ${GCP_K8S_CREDENTIALS} > /gcp/gcp-credentials.json
@@ -29,16 +17,22 @@ helm init --client-only
 
 helm repo add praqma https://praqma-helm-repo.s3.amazonaws.com/
 
-helm upgrade \
-  --install \
-  --namespace=${NAMESPACE} \
-  --set-string containers[0].image=${IMAGE} \
-  --set-string containers[0].tag=${TAG} \
-  --set service.port=${PORT} \
-  --set containers[0].livenessProbe.port=${PORT} \
-  --set containers[0].readinessProbe.port=${PORT} \
-  --set-string service.annotations."prometheus\.io/port"=${PORT} \
-  --values .circleci/custom-start-point-values.yaml \
-  ${NAMESPACE}-custom-start-points \
-  praqma/cyber-dojo-service \
-  --version 0.2.5
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Normally I export the cyberdojo env-vars using the command
+# $ docker run --rm cyberdojo/versioner:latest
+# This won't work on the.circleci deployment step since it is
+# run inside the cyberdojo/gcloud-kubectl-helm image which does
+# not have docker. So doing it directly from versioner's git repo
+export $(curl https://raw.githubusercontent.com/cyber-dojo/versioner/master/app/.env)
+
+readonly NAMESPACE="${1}" # beta | prod
+readonly CYBER_DOJO_CUSTOM_START_POINTS_TAG="${CIRCLE_SHA1:0:7}"
+
+helm_upgrade \
+   "${NAMESPACE}" \
+   "${CYBER_DOJO_CUSTOM_START_POINTS_IMAGE}" \
+   "${CYBER_DOJO_CUSTOM_START_POINTS_TAG}" \
+   "${CYBER_DOJO_CUSTOM_START_POINTS_PORT}" \
+   ".circleci/custom-start-point-values.yml" \
+   "custom-start-points" \
+   "praqma/cyber-dojo-service --version 0.2.5"
