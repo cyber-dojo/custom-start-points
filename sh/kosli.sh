93d70465578342cf14aa86b0333319819ce7bff9
JonJagger <jon@kosli.com>
2024-02-10 19:42:31 +0000

    Fix kosli.sh script so you can run locally [ci skip]

--- sh/kosli.sh
+++ sh/kosli.sh
@@ -3,7 +3,7 @@ set -Eeu
 
 export KOSLI_ORG=cyber-dojo
 export KOSLI_FLOW=custom-start-points-ci
-export KOSLI_TRAIL="${GITHUB_SHA}"
+export KOSLI_TRAIL="$(git rev-parse HEAD)"
 
 # KOSLI_ORG is set in CI
 # KOSLI_API_TOKEN is set in CI
